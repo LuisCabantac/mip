ca9bcd284a8783f4bed91a1ca6a350089aa1c5c0
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-11-11 15:25:15 +0800

    feat: Add geolocationDataSchema for validating geolocation data structure

--- src/lib/schema/index.ts
+++ src/lib/schema/index.ts
@@ -3,3 +3,23 @@ import z from "zod";
 export const searchIPSchema = z.object({
   ip: z.union([z.ipv4(), z.ipv6()]),
 });
+
+export const geolocationDataSchema = z.object({
+  ip: z.string(),
+  hostname: z.string(),
+  city: z.string(),
+  region: z.string(),
+  country: z.string(),
+  loc: z.string(),
+  org: z.string(),
+  postal: z.string(),
+  timezone: z.string(),
+  asn: z.string(),
+  as_name: z.string(),
+  as_domain: z.string(),
+  country_code: z.string(),
+  continent_code: z.string(),
+  continent: z.string(),
+});
+
+export type GeolocationData = z.infer<typeof geolocationDataSchema>;
