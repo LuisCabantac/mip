ed29f0f7eb07606c3ef10ad32d64d0d9fa6226db
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-11-12 05:53:17 +0800

    feat: Update Home component with history management functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,51 @@
 import { Metadata } from "next";
 
 import HomePage from "@/components/HomePage";
-import { createHistory } from "@/lib/services/history";
+import {
+  createHistory,
+  deleteMultipleHistory,
+  getAllHistory,
+} from "@/lib/services/history";
 import { GeolocationData } from "@/lib/schema";
+import { getUserData, isAuthenticated } from "@/lib/auth";
 
 export const metadata: Metadata = {
   title: "Home",
   description: "Welcome to mIP.",
 };
 
-export default function Home() {
+export default async function Home() {
+  const authenticated = await isAuthenticated();
+  const userData = await getUserData();
+
   async function handleCreateHistory(geolocationData: GeolocationData) {
     "use server";
 
     await createHistory(geolocationData);
     return;
   }
-  return <HomePage onCreateHistory={handleCreateHistory} />;
+
+  async function handleDeleteHistory(ids: string[]) {
+    "use server";
+
+    await deleteMultipleHistory(ids);
+    return;
+  }
+
+  async function handleGetAllHistory() {
+    "use server";
+
+    const response = await getAllHistory();
+    return response;
+  }
+
+  return (
+    <HomePage
+      onCreateHistory={handleCreateHistory}
+      isAuthenticated={authenticated}
+      userData={userData}
+      onGetAllHistory={handleGetAllHistory}
+      onDeleteHistory={handleDeleteHistory}
+    />
+  );
 }
